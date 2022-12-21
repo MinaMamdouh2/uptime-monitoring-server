0ae7fe64bb86fe5f1c95172c2d0ca62e164b2fae
MinaMamdouh2 <sxgs555@gmail.com>
2022-12-21 13:25:46 +0200

    Added prestart node script Added utils folder Added database intialization

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 // Creating a db configuration object
 const dbConfig = {
   DB_HOST: process.env.DB_HOST,
-  DB_USER: process.env.root,
+  DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DATABASE_NAME: process.env.DATABASE_NAME,
   DB_PORT: process.env.DB_PORT,
